40e65a0dfabbab01f303d0efacc1b85fef31e377
tonythethompson <tonythethompson@hotmail.com>
2026-07-06 12:28:48 -0700

    checkpoint before checking out master

--- QuickShell.Raycast/raycast-env.d.ts
+++ QuickShell.Raycast/raycast-env.d.ts
@@ -1,1 +1,36 @@
-/// <reference types="@raycast/api" />
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `open-workspace` command */
+  export type OpenWorkspace = ExtensionPreferences & {}
+  /** Preferences accessible in the `create-workspace` command */
+  export type CreateWorkspace = ExtensionPreferences & {}
+  /** Preferences accessible in the `edit-workspace` command */
+  export type EditWorkspace = ExtensionPreferences & {}
+  /** Preferences accessible in the `settings` command */
+  export type Settings = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `open-workspace` command */
+  export type OpenWorkspace = {}
+  /** Arguments passed to the `create-workspace` command */
+  export type CreateWorkspace = {}
+  /** Arguments passed to the `edit-workspace` command */
+  export type EditWorkspace = {}
+  /** Arguments passed to the `settings` command */
+  export type Settings = {}
+}
+
